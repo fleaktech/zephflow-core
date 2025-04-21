2eb83b3ee3ef5dd541851c40cefd7752a729a560
Bo Lei <bo@fleak.ai>
2025-04-21 13:44:15 -0700

    fixed license task

--- sdk/src/main/java/io/fleak/zephflow/sdk/ZephFlow.java
+++ sdk/src/main/java/io/fleak/zephflow/sdk/ZephFlow.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025 Fleak Tech Inc. // TODO: Update copyright year
+ * Copyright 2025 Fleak Tech Inc.
  *
  * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
