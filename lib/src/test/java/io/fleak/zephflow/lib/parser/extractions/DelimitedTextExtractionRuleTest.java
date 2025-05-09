241c68bdd5d898a4696ff36a258d5b709a6251b2
Bo Lei <bo@fleak.ai>
2025-05-09 13:54:56 -0700

    delimited extractor (#20)

---
+++ lib/src/test/java/io/fleak/zephflow/lib/parser/extractions/DelimitedTextExtractionRuleTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2025 Fleak Tech Inc.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fleak.zephflow.lib.parser.extractions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.fleak.zephflow.api.structure.RecordFleakData;
+import io.fleak.zephflow.lib.utils.MiscUtils;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
+
+/** Created by bolei on 5/8/25 */
+class DelimitedTextExtractionRuleTest {
+
+  @Test
+  void extract() throws Exception {
+    String log = MiscUtils.loadStringFromResource("/parser/snare_windows.txt");
+    //noinspection ExtractMethodRecommender
+    DelimitedTextExtractionRule delimitedTextExtractionRule =
+        new DelimitedTextExtractionRule(
+            "\t",
+            List.of(
+                "Hostname",
+                "SnareLogType",
+                "Criticality",
+                "EventLogSource",
+                "SnareEventCounter",
+                "SubmitTime",
+                "EventCode",
+                "SourceName",
+                "UserName",
+                "SIDType",
+                "WindowsLogType",
+                "ComputerName",
+                "CategoryString",
+                "DataString",
+                "Message",
+                "EventLogCounter"));
+    RecordFleakData parsed = delimitedTextExtractionRule.extract(log);
+    assertEquals(
+        ImmutableMap.builder()
+            .put("Hostname", "generic-server.example.com")
+            .put("SnareLogType", "MSWinEventLog")
+            .put("Criticality", "0")
+            .put("EventLogSource", "Security")
+            .put("SnareEventCounter", "[LogEntryID]")
+            .put("SubmitTime", "[Timestamp]")
+            .put("EventCode", "4657")
+            .put("SourceName", "Microsoft-Windows-Security-Auditing")
+            .put("UserName", "EXAMPLE_DOMAIN\\GENERIC-SERVER$")
+            .put("SIDType", "N/A")
+            .put("WindowsLogType", "Success Audit")
+            .put("ComputerName", "generic-server.example.com")
+            .put("CategoryString", "Registry")
+            .put("DataString", "")
+            .put(
+                "Message",
+                "A registry value was modified.  Subject: Security ID: S-1-5-18 Account Name: GENERIC-SERVER$ Account Domain: EXAMPLE_DOMAIN Logon ID: [LogonID]  Object: Object Name: \\REGISTRY\\MACHINE\\SOFTWARE\\Vendor\\ProductName\\Status Object Value Name: LOG_SETTING_NAME Handle ID: [HandleID] Operation Type: Existing registry value modified  Process Information: Process ID: [ProcessID] Process Name: C:\\Program Files\\ApplicationFolder\\Application.exe  Change Information: Old Value Type: REG_SZ Old Value: <BookmarkList>  <Bookmark Channel='Security' RecordId='[OldRecordID]' IsCurrent='true'/> </BookmarkList> New Value Type: REG_SZ New Value: <BookmarkList>  <Bookmark Channel='Security' RecordId='[NewRecordID]' IsCurrent='true'/> </BookmarkList>")
+            .put("EventLogCounter", "0")
+            .build(),
+        parsed.unwrap());
+  }
+}
