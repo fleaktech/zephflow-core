8dbf6ea6be9020442f51755ad5691eed54cf6cc9
Dawei <dawei@fleak.ai>
2025-08-08 13:03:37 +0800

    add jar version log

--- clistarter/src/main/java/io/fleak/zephflow/clistarter/Main.java
+++ clistarter/src/main/java/io/fleak/zephflow/clistarter/Main.java
@@ -29,6 +29,9 @@ import org.influxdb.dto.Query;
 public class Main {
 
   public static void main(String[] args) throws Exception {
+    log.info("=== Application Version Information ===");
+    log.info("CLI Starter JAR: {}", System.getenv("CLISTARTER_JAR_FILENAME"));
+    log.info("Commands JAR: {}", System.getenv("COMMANDS_JAR_FILENAME"));
     try {
       JobConfig jobConfig = JobCliParser.parseArgs(args);
       MetricClientProvider metricClientProvider = createMetricClientProvider(args);
