963a69d3b390803af4606c56c1dd3fa202b46044
Dawei <dawei@fleak.ai>
2025-08-09 20:56:22 +0800

    remove command jar version log

--- clistarter/src/main/java/io/fleak/zephflow/clistarter/Main.java
+++ clistarter/src/main/java/io/fleak/zephflow/clistarter/Main.java
@@ -31,7 +31,6 @@ public class Main {
   public static void main(String[] args) throws Exception {
     log.info("=== Application Version Information ===");
     log.info("CLI Starter JAR: {}", System.getenv("CLISTARTER_JAR_FILENAME"));
-    log.info("Commands JAR: {}", System.getenv("COMMANDS_JAR_FILENAME"));
     try {
       JobConfig jobConfig = JobCliParser.parseArgs(args);
       MetricClientProvider metricClientProvider = createMetricClientProvider(args);
