b6bd091e2108e87adedffe898bdefdfdfa6d17f3
Dan <dan@fleak.ai>
2026-04-15 17:46:46 +0300

    Fixed tests

--- lib/src/test/java/io/fleak/zephflow/lib/commands/OperatorCommandRegistryTest.java
+++ lib/src/test/java/io/fleak/zephflow/lib/commands/OperatorCommandRegistryTest.java
@@ -47,6 +47,8 @@ class OperatorCommandRegistryTest {
             .add(COMMAND_NAME_DATABRICKS_SINK)
             .add(COMMAND_NAME_SYSLOG_UDP)
             .add(COMMAND_NAME_ACTIVEMQ_SOURCE)
+            .add(COMMAND_NAME_SQS_SINK)
+            .add(COMMAND_NAME_SQS_SOURCE)
             .build(),
         OPERATOR_COMMANDS.keySet());
   }
