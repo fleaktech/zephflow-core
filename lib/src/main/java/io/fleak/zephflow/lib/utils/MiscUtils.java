e1435fa1f36fd6499c541a64fa5337e7e80501d4
Dan <dan@fleak.ai>
2026-04-16 16:28:32 +0300

    FLE-1519: Add Microsoft Sentinel sink connector

--- lib/src/main/java/io/fleak/zephflow/lib/utils/MiscUtils.java
+++ lib/src/main/java/io/fleak/zephflow/lib/utils/MiscUtils.java
@@ -100,6 +100,7 @@ public interface MiscUtils {
   String COMMAND_NAME_LDAP_SOURCE = "ldapsource";
   String COMMAND_NAME_AZURE_MONITOR_SINK = "azuremonitorsink";
   String COMMAND_NAME_AZURE_MONITOR_SOURCE = "azuremonitorsource";
+  String COMMAND_NAME_SENTINEL_SINK = "sentinelsink";
   String METRIC_NAME_INPUT_EVENT_COUNT = "input_event_count";
   String METRIC_NAME_INPUT_EVENT_SIZE_COUNT = "input_event_size";
   String METRIC_NAME_INPUT_DESER_ERR_COUNT = "input_deser_err_count";
