25804877bb4a8442fded930dbfff975636e75a98
Dan <dan@fleak.ai>
2026-04-24 17:49:11 +0300

    Review & tests

--- lib/src/main/java/io/fleak/zephflow/lib/azure/AzureClientFactory.java
+++ lib/src/main/java/io/fleak/zephflow/lib/azure/AzureClientFactory.java
@@ -13,17 +13,51 @@
  */
 package io.fleak.zephflow.lib.azure;
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.okhttp.OkHttpAsyncHttpClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.ConnectionPool;
+import okhttp3.Dispatcher;
+import okhttp3.OkHttpClient;
 
 @Slf4j
 public class AzureClientFactory {
 
+  private static final HttpClient HTTP_CLIENT = buildDaemonHttpClient();
+
+  private static HttpClient buildDaemonHttpClient() {
+    ThreadPoolExecutor executor =
+        new ThreadPoolExecutor(
+            0,
+            Integer.MAX_VALUE,
+            60,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            r -> {
+              Thread t = new Thread(r, "azure-okhttp");
+              t.setDaemon(true);
+              return t;
+            });
+    OkHttpClient okHttpClient =
+        new OkHttpClient.Builder()
+            .connectionPool(new ConnectionPool(5, 5, TimeUnit.MINUTES))
+            .dispatcher(new Dispatcher(executor))
+            .build();
+    return new OkHttpAsyncHttpClientBuilder(okHttpClient).build();
+  }
+
   public BlobServiceClient createBlobServiceClientFromConnectionString(String connectionString) {
     log.info("Creating Azure BlobServiceClient using connection string");
-    return new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+    return new BlobServiceClientBuilder()
+        .httpClient(HTTP_CLIENT)
+        .connectionString(connectionString)
+        .buildClient();
   }
 
   public BlobServiceClient createBlobServiceClientFromAccountKey(
@@ -31,6 +65,7 @@ public class AzureClientFactory {
     log.info("Creating Azure BlobServiceClient for account: {}", accountName);
     StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
     return new BlobServiceClientBuilder()
+        .httpClient(HTTP_CLIENT)
         .endpoint("https://" + accountName + ".blob.core.windows.net")
         .credential(credential)
         .buildClient();
