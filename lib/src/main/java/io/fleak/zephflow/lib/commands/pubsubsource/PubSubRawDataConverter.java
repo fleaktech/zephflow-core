b7c1b8ad50d60626940da21625c1a151bd76accb
Dan <dan@fleak.ai>
2026-05-01 14:29:07 +0300

    Add PubSub source raw data types, encoder, and converter

---
+++ lib/src/main/java/io/fleak/zephflow/lib/commands/pubsubsource/PubSubRawDataConverter.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2025 Fleak Tech Inc.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fleak.zephflow.lib.commands.pubsubsource;
+
+import static io.fleak.zephflow.lib.utils.JsonUtils.toJsonString;
+import static io.fleak.zephflow.lib.utils.MiscUtils.getCallingUserTagAndEventTags;
+
+import io.fleak.zephflow.api.structure.RecordFleakData;
+import io.fleak.zephflow.lib.commands.source.ConvertedResult;
+import io.fleak.zephflow.lib.commands.source.RawDataConverter;
+import io.fleak.zephflow.lib.commands.source.SourceExecutionContext;
+import io.fleak.zephflow.lib.serdes.SerializedEvent;
+import io.fleak.zephflow.lib.serdes.des.FleakDeserializer;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PubSubRawDataConverter implements RawDataConverter<PubSubReceivedMessage> {
+
+  private final FleakDeserializer<?> fleakDeserializer;
+
+  public PubSubRawDataConverter(FleakDeserializer<?> fleakDeserializer) {
+    this.fleakDeserializer = fleakDeserializer;
+  }
+
+  @Override
+  public ConvertedResult<PubSubReceivedMessage> convert(
+      PubSubReceivedMessage sourceRecord, SourceExecutionContext<?> sourceInitializedConfig) {
+    try {
+      SerializedEvent serializedEvent =
+          new SerializedEvent(null, sourceRecord.body(), sourceRecord.attributes());
+      List<RecordFleakData> events = fleakDeserializer.deserialize(serializedEvent);
+
+      Map<String, String> eventTags =
+          getCallingUserTagAndEventTags(null, events.isEmpty() ? null : events.getFirst());
+
+      sourceInitializedConfig.dataSizeCounter().increase(sourceRecord.body().length, eventTags);
+      sourceInitializedConfig.inputEventCounter().increase(events.size(), eventTags);
+      if (log.isDebugEnabled()) {
+        events.forEach(e -> log.debug("got message: {}", toJsonString(e)));
+      }
+      return ConvertedResult.success(events, sourceRecord);
+    } catch (Exception e) {
+      sourceInitializedConfig.dataSizeCounter().increase(sourceRecord.body().length, Map.of());
+      sourceInitializedConfig.deserializeFailureCounter().increase(Map.of());
+      log.error("failed to deserialize Pub/Sub message: {}", sourceRecord.messageId());
+      return ConvertedResult.failure(e, sourceRecord);
+    }
+  }
+}
